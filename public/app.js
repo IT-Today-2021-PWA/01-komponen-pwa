88aa9055d5359c9b82bf0cb513d6fb3ac59b4218
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2021-09-05 17:48:48 +0200

    feat: add basic service worker

--- public/app.js
+++ public/app.js
@@ -1,5 +1,9 @@
-function app() {
-  console.log('Welcome to IT Today 2021!');
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+    console.log('Instalasi service worker sukses:', registration)
+  }, function (error) {
+    console.log('Instalasi service worker gagal:', error)
+  })
+} else {
+  console.log('Service worker tidak didukung.')
 }
-
-app();
